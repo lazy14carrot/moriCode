790c42810c18d4357af58f9e780f7dff5d593529
M. Kim <beta33miri@naver.com>
2015-03-03 18:01:50 +0000

    ver 0.0

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@
 //  Copyright (c) 2015 crayon. All rights reserved.
 //
 
-#include <iostream>
+#include <iostream>     //  cout
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    cout << "Hello, World!\n";
     return 0;
 }
