1ca7240b8f4068bc83b964048213feb2fe833025
M. Kim <beta33miri@naver.com>
2015-03-03 17:56:22 +0000

    ver0.0

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EpiNotes
+//
+//  Created by crayon on 03/03/2015.
+//  Copyright (c) 2015 crayon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
